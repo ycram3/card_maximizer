315f08caedbaf34626b9f86b71e90ed5b003cc93
ycram3 <marcellorusciano@gmail.com>
2022-11-04 20:41:25 -0700

    logic for switching between categories added. To be added the entire list of the categories

--- card_maximizer/card_maximizer/ViewController.swift
+++ card_maximizer/card_maximizer/ViewController.swift
@@ -8,7 +8,43 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    lazy var responseFromGoogle: String = "restaurant"
+    
+    var storeType = ["restaurant","grocery","gas"] //needs to pass the store type from maps
 
+    func getCategory(storeType: [String]) -> String{
+        var category : String = "Unknown"
+        var n = 0
+        for _ in storeType {
+            if storeType[n] == responseFromGoogle {
+                category = "dining"
+            }
+            n = n + 1
+        }
+        print("Inside getCategory ",category)
+        return category
+    }
+    
+    lazy var storeCategory: String = getCategory(storeType: storeType)
+
+    @IBOutlet weak var creditCard: UILabel!
+    
+    @IBAction func confirmStore(_ sender: UIButton) {
+        showTheCardToBeUsed(storeCategory: storeCategory)
+    }
+    
+    
+    func showTheCardToBeUsed(storeCategory: String){
+        switch storeCategory{
+        case "dining": creditCard.text = "Discover"
+            default:
+                print("I am here instead")
+        }
+    }
+
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
