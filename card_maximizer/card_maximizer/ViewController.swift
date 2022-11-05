bd612144f8c6a459716d30ec0aee7ab98a5df6ef
ycram3 <marcellorusciano@gmail.com>
2022-11-05 00:56:29 -0700

    dictonary added,Button colored,Updated UI

--- card_maximizer/card_maximizer/ViewController.swift
+++ card_maximizer/card_maximizer/ViewController.swift
@@ -9,36 +9,52 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    lazy var responseFromGoogle: String = "restaurant"
+    lazy var responseFromGoogle: String = "bakery"
     
-    var storeType = ["restaurant","grocery","gas"] //needs to pass the store type from maps
+    var dict = ["amusement_park":"entertainment",
+                "aquarium":"entertainment",
+                "bakery":"dining",
+                "bar":"dining",
+                "bowling_alley":"entertainment",
+                "cafe":"dining",
+                "car_rental":"travel",
+                "gas_station":"gas",
+                "museum":"entertainment",
+                "night_club":"dining",
+                "restaurant":"dining",
+                "supermarket":"grocery",
+                "tourist_attraction":"entertainment",
+                "zoo":"entertainment"]
 
-    func getCategory(storeType: [String]) -> String{
+    func getCategory() -> String {
         var category : String = "Unknown"
-        var n = 0
-        for _ in storeType {
-            if storeType[n] == responseFromGoogle {
-                category = "dining"
+        for key in dict.keys{
+            if responseFromGoogle == key{
+               category = dict[key]!
             }
-            n = n + 1
         }
-        print("Inside getCategory ",category)
         return category
     }
-    
-    lazy var storeCategory: String = getCategory(storeType: storeType)
+    lazy var storeCategory: String = getCategory()
+
+    @IBOutlet weak var creditCardNumber: UILabel!
 
     @IBOutlet weak var creditCard: UILabel!
     
     @IBAction func confirmStore(_ sender: UIButton) {
         showTheCardToBeUsed(storeCategory: storeCategory)
+        sender.backgroundColor = UIColor.green
     }
     
     
     func showTheCardToBeUsed(storeCategory: String){
         switch storeCategory{
-        case "dining": creditCard.text = "Discover"
-            default:
+        case "dining": creditCard.text = "Discover"; creditCardNumber.text = "6011 0009 9013 9424"
+        case "entertainment": creditCard.text = "Chase"
+        case "travel": creditCard.text = "Amex"
+        case "gas": creditCard.text = "Bank of America"
+        case "grocery": creditCard.text = "Capitol One"
+            default: creditCard.text = "Wells Fargo"
                 print("I am here instead")
         }
     }
